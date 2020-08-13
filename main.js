d6f48d09763f512a8c4097ebac9451533f2e3dd8
Mykty-Ayan <akkassov.ayan@gmail.com>
2020-08-14 01:37:13 +0600

    Changed to relevant experience

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const timezone = Intl.DateTimeFormat.resolvedOptions().timezone()
-function changeBodyBackgroundByTime(currentTime){
-    print()
+// const timezone = Intl.DateTimeFormat.resolvedOptions().timezone()
+// function changeBodyBackgroundByTime(currentTime){
+//     print()
+// }
+
+function myFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event){
+    if(event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let index = 0; index < dropdowns.length; index++) {
+            var openDropdown = dropdowns[index];
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
